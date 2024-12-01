00afe2da1c62c1dca4ae4be36822be44341569aa
Ata Çınar Genç <atacinargenc@MacBook-Air.local>
2024-12-01 13:58:21 -0500

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/coffee-grinder-converter',
+  assetPrefix: '/coffee-grinder-converter/',
+}
 
-export default nextConfig;
+export default nextConfig
